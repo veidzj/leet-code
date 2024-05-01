b7ec184065b5bcb688158928c159c0fb69190ab6
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2024-05-01 15:13:03 -0300

    feat(js): 872

---
+++ js/872.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const middleNode = function(head) {
+  let middle = head
+  let end = head
+
+  while (end && end.next) {
+    middle = middle.next
+    end = end.next.next
+  }
+
+  return middle
+}
+
+console.log(middleNode([1,2,3,4,5]))
